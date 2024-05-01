63900988dcb1aae3bbfdb1789852cf1d1870d2b4
rabbyxq <rabbyxq@gmail.com>
2024-05-01 10:06:16 +0600

    Update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer  = require('multer');
+const path = require('path');
 const userRoutes = require('./routes/user');
 const pool = require('./config/db');
 const sessionMiddleware = require('./middlewares/sessionMiddleware');
@@ -16,6 +18,29 @@ app.use(bodyParser.json());
 
 app.use('/users', userRoutes);
 app.use(dashboardRoutes);
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/') // Files will be saved in the 'uploads/' directory
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname) // Use the original file name as the file name
+  }
+});
+const upload = multer({ storage });
+
+// Serve static files from the 'uploads/' directory
+app.use(express.static(path.join(__dirname, 'uploads')));
+
+// Route to handle file upload
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+  res.status(200).send('File uploaded successfully: ' + req.file.filename);
+});
+
 
 
 
